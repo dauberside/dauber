6200be8cb4b106ab268ad2682be85c2c2a079a31
DauberSide <daubersideinfo@gmail.com>
2026-08-27 02:11:15 +0900

    fix: restore public site styling

---
+++ WEB/thx38/src/pages/_app.tsx
@@ -0,0 +1,33 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "@/styles/globals.css";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+import type { AppProps } from "next/app";
+import { Noto_Sans_JP } from "next/font/google";
+import Head from "next/head";
+import React from "react";
+
+const notoSans = Noto_Sans_JP({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-sans",
+  display: "swap",
+});
+
+config.autoAddCss = false;
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>dauberside</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className={notoSans.variable}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
+}
+
+export default App;
